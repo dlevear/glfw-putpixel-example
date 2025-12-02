9f9f03988239df21ecad35449334f519f2c65406
dlevear <dlevear@gmail.com>
2025-12-02 17:38:32 -0500

    Added prototypes so main() can go at the top. Also added more comments and renamed some things for clarity.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// For build system, see https://github.com/dlevear/glfw-putpixel-example
 #define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -6,10 +7,56 @@ using std::cout, std::endl;
 using std::string, std::to_string;
 
 // Globals
-float* image;
+float* canvas;
 float prev_draw = 0; // for calculating fps
 
-// Initialization function to contain some GLFW calls
+// Use put_pixel() to set the color of the pixels
+void put_pixel(int row, int col, float r, float g, float b);
+
+// Initialization function to contain some GLFW calls. Call once.
+GLFWwindow* init();
+
+// Drawing function to contain GLFW calls. Call once per frame.
+void update_window(GLFWwindow* window);
+
+int main() {
+    // Locals
+    GLFWwindow* window = init();
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Handling events, e.g. key presses
+        glfwPollEvents();
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+            glfwSetWindowShouldClose(window, true);
+        }
+
+        // Pixel updates
+        // (drawing a 100 x 100 pink rectangle for example)
+        for (int col=0; col < 100; col++) {
+            for (int row=0; row < 100; row++) {
+                put_pixel(row, col, 0.99, 0.41, 0.70);
+            }
+        }
+
+        update_window(window);
+    }
+
+    glfwTerminate();
+    delete [] canvas;
+    return 0;
+}
+
+void put_pixel(int row, int col, float r, float g, float b) {
+    if (row >= 480 || col >= 640 || row < 0 || col < 0) {
+        cout << "(row,col) = (" << row << "," << col << ") is not valid for putPixel()" << endl;
+        return;
+    }
+    canvas[640*3*(480-1-row) + 3*col + 0] = r;
+    canvas[640*3*(480-1-row) + 3*col + 1] = g;
+    canvas[640*3*(480-1-row) + 3*col + 2] = b;
+}
+
 GLFWwindow* init() {
     // Readying GLFW and Window
     glfwInit();
@@ -24,65 +71,27 @@ GLFWwindow* init() {
     }
     glfwMakeContextCurrent(window);
 
-    return window;
-}
-
-// Drawing function to contain GLFW calls
-void draw(GLFWwindow* window) {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glDrawPixels(640, 480, GL_RGB, GL_FLOAT, (void*) image);
-    glfwSwapBuffers(window);
-}
-
-// Use this to draw anything
-void put_pixel(int row, int col, float r, float g, float b) {
-    if (row >= 480 || col >= 640 || row < 0 || col < 0) {
-        cout << "(row,col) = (" << row << "," << col << ") is not valid for putPixel()" << endl;
-        return;
-    }
-    image[640*3*(480-1-row) + 3*col + 0] = r;
-    image[640*3*(480-1-row) + 3*col + 1] = g;
-    image[640*3*(480-1-row) + 3*col + 2] = b;
-}
-
-int main() {
-    // Initializing image
-    image = new float[640*480*3];
+    // Initializing canvas
+    canvas = new float[640*480*3];
     for (int col=0; col < 640; col++) {
         for (int row=0; row < 480; row++) {
             put_pixel(row, col, 1, 1, 1);
         }
     }
 
-    GLFWwindow* window = init();
-    while (!glfwWindowShouldClose(window)) {
-        // Calculating fps
-        double elapsed = glfwGetTime() - prev_draw;
-        double fps = 1/elapsed;
-        string title = "fps: " + to_string((int) fps);
-        glfwSetWindowTitle(window, title.c_str());
+    return window;
+}
 
-        // Events
-        glfwPollEvents();
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE)) {
-            glfwSetWindowShouldClose(window, true);
-        }
-
-        // Drawing a 100 x 100 pink rectangle for example
-        for (int col=0; col < 100; col++) {
-            for (int row=0; row < 100; row++) {
-                put_pixel(row, col, 0.99, 0.41, 0.70);
-            }
-        }
-
-        // Drawing
-        prev_draw = glfwGetTime();
-        draw(window);
-    }
-
-    glfwTerminate();
-    delete [] image;
-    return 0;
+void update_window(GLFWwindow* window) {
+    // Calculating fps
+    double elapsed = glfwGetTime() - prev_draw;
+    double fps = 1/elapsed;
+    string title = "fps: " + to_string((int) fps);
+    glfwSetWindowTitle(window, title.c_str());
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawPixels(640, 480, GL_RGB, GL_FLOAT, (void*) canvas);
+    glfwSwapBuffers(window);
+    prev_draw = glfwGetTime();
 }
 
 
